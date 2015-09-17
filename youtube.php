12e199066f27fab6aefa8198ea7b7a57f37602f6
Jeremy Leggat <jleggat@asu.edu>
2015-09-17 15:27:12 -0700

    fix youtube link.

--- youtube.php
+++ youtube.php
@@ -5,5 +5,5 @@
 
 <div>
 <p style="font-weight: bold;">
-<a href="//www.youtube.com/cronkitenews" style="font-size: 14px; color: #444; text-decoration: none;">Cronkite News on <img src="<?php bloginfo('template_directory'); ?>/img/ytlogo-red.png" height="50" alt="YouTube logo" style="vertical-align: middle;" /></a></p>
+<a href="https://www.youtube.com/cronkitenews" style="font-size: 14px; color: #444; text-decoration: none;">Cronkite News on <img src="<?php bloginfo('template_directory'); ?>/img/ytlogo-red.png" height="50" alt="YouTube logo" style="vertical-align: middle;" /></a></p>
 </div>
