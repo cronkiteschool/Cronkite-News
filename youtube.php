e2e13a254eb7ff051344f83223b7a7fdef6c5757
Jeremy Leggat <jleggat@asu.edu>
2015-09-18 13:19:08 -0700

    Update headlines link
    Point link to current site form.

--- youtube.php
+++ youtube.php
@@ -1,9 +1,12 @@
 <div>
-<p style="font-weight: bold; margin-bottom: 0;"><a href="daily-newsletter-signup" style="font-size: 14px; color: #234384; text-decoration: none;">Sign up for daily headlines</a></p>
+<p style="font-weight: bold; margin-bottom: 0;">
+<a href="http://cronkitenews.azpbs.org/daily-newsletter-signup" style="font-size: 14px; color: #234384; text-decoration: none;">Sign up for daily headlines</a>
+</p>
 </div>
 <hr style="margin-bottom: 5px; margin-top: 5px;" />
 
 <div>
 <p style="font-weight: bold;">
-<a href="https://www.youtube.com/cronkitenews" style="font-size: 14px; color: #444; text-decoration: none;">Cronkite News on <img src="<?php bloginfo('template_directory'); ?>/img/ytlogo-red.png" height="50" alt="YouTube logo" style="vertical-align: middle;" /></a></p>
+<a href="https://www.youtube.com/cronkitenews" style="font-size: 14px; color: #444; text-decoration: none;">Cronkite News on <img src="<?php bloginfo('template_directory'); ?>/img/ytlogo-red.png" height="50" alt="YouTube logo" style="vertical-align: middle;" /></a>
+</p>
 </div>
